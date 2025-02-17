3b30f5a4926bfd2dd562a25893f9c507aa6ef302
Mohd Shoaib <142038696+shoaib0912@users.noreply.github.com>
2025-02-17 09:51:47 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,17 @@
+//pattern program
+#include<stdio.h>
+int main()
+{
+	int i,j,num;
+	printf("Enter the number of rows:");
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
